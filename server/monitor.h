19e05c8d0ea8a36faabbb38d25ddb9b7d27b8c75
Ben Harper <rogojin@gmail.com>
2020-08-19 08:32:41 +0200

    Reduce overload threshold from 2950 to 2500
    It's just not worth it risking blowing the inverter,
    for a few more cents of PV generation.

--- server/monitor.h
+++ server/monitor.h
@@ -18,7 +18,7 @@ public:
 
 	int                SampleWriteInterval    = 20;   // Write to database once every N samples.
 	int                SecondsBetweenSamples  = 5;    // Record data every N seconds
-	int                OverloadThresholdWatts = 2950; // The inverter is overloaded if the output load goes beyond this
+	int                OverloadThresholdWatts = 2500; // The inverter is overloaded if the output load goes beyond this
 	int                GridVoltageThreshold   = 200;  // Grid voltage below this is considered "grid off"
 	std::atomic<bool>  IsInitialized;                 // Set to true once we've made our first successful reading
 	std::atomic<bool>  IsOverloaded;                  // Signalled when inverter usage is higher than OverloadThresholdWatts
